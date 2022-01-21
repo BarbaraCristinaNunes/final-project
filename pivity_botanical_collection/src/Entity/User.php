788f226e54a8fb96cc441c89fa9b2f9156d2b7cb
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-01-21 14:49:19 +0100

    Create User.php

---
+++ pivity_botanical_collection/src/Entity/User.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $instituition;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $laboratory;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $username;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $email;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $password;
+
+    #[ORM\Column(type: 'boolean')]
+    private $adm;
+
+    #[ORM\Column(type: 'boolean')]
+    private $online;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getInstituition(): ?string
+    {
+        return $this->instituition;
+    }
+
+    public function setInstituition(string $instituition): self
+    {
+        $this->instituition = $instituition;
+
+        return $this;
+    }
+
+    public function getLaboratory(): ?string
+    {
+        return $this->laboratory;
+    }
+
+    public function setLaboratory(string $laboratory): self
+    {
+        $this->laboratory = $laboratory;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getAdm(): ?bool
+    {
+        return $this->adm;
+    }
+
+    public function setAdm(string $adm): self
+    {
+        $this->adm = $adm;
+
+        return $this;
+    }
+
+    public function getOnline(): ?bool
+    {
+        return $this->online;
+    }
+
+    public function setOnline(bool $online): self
+    {
+        $this->online = $online;
+
+        return $this;
+    }
+}
