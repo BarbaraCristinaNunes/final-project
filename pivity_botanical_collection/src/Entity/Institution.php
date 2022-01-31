ba87193d977be71079d79a6012f0c4b1a60c6595
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-01-31 10:37:02 +0100

    Create Institution.php

---
+++ pivity_botanical_collection/src/Entity/Institution.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InstitutionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InstitutionRepository::class)]
+class Institution
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $laboratory;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLaboratory(): ?string
+    {
+        return $this->laboratory;
+    }
+
+    public function setLaboratory(string $laboratory): self
+    {
+        $this->laboratory = $laboratory;
+
+        return $this;
+    }
+}
