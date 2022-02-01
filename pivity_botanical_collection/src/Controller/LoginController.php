7a3ce001fd92d1e76472318cc0e16e226ebeea6a
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-02-01 11:17:57 +0100

    created loginValidation method

--- pivity_botanical_collection/src/Controller/LoginController.php
+++ pivity_botanical_collection/src/Controller/LoginController.php
@@ -5,14 +5,43 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Institution;
+use App\Repository\InstitutionRepository;
+use App\Entity\User;
+use App\Repository\UserRepository;
 
 class LoginController extends AbstractController
 {
+
+    private string $message = "";
+
     #[Route('/login', name: 'login')]
     public function index(): Response
     {
         return $this->render('login/index.html.twig', [
             'controller_name' => 'LoginController',
+            'message' => $this->message,
         ]);
     }
+
+    #[Route('/userLogin', name: 'userLogin')]
+    public function loginValidation(Request $request): Response
+    {
+        $institution = $request->request->get('institution');
+        $laboratory = $request->request->get('laboratory');
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+
+        
+
+        var_dump($institution, $laboratory, $email, $password);
+
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
+            'message' => $this->message,
+        ]);
+    }
+
 }
