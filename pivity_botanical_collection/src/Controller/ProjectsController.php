0c91b2f159c012e27558a6e47de008c9bfb619b4
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-02-09 11:53:57 +0100

    now I can create a project
    I did not do the validation yet

--- pivity_botanical_collection/src/Controller/ProjectsController.php
+++ pivity_botanical_collection/src/Controller/ProjectsController.php
@@ -5,12 +5,35 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Project;
+use App\Repository\ProjectRepository;
 
 class ProjectsController extends AbstractController
 {
+    private string $message = "";
     #[Route('/projects', name: 'projects')]
     public function index(): Response
     {
+        return $this->render('projects/index.html.twig', [
+            'controller_name' => 'ProjectsController',
+        ]);
+    }
+
+    #[Route('/createProject', name: 'createProject')]
+    public function createProject(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $name = $request->request->get('projectName');
+        $coordinator = $request->request->get('coordinator');
+        $fundingInstitution = $request->request->get('funding');
+
+        var_dump($name, $coordinator, $fundingInstitution);
+
+        $project = new Project($name, $coordinator, $fundingInstitution, 1);
+
+        ProjectRepository::createProject($doctrine, $project);
+
         return $this->render('projects/index.html.twig', [
             'controller_name' => 'ProjectsController',
         ]);
