0a27315cb399a67d8cf2e984c0f4e3a920060110
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-02-10 11:39:36 +0100

    I can show all subprojects

--- pivity_botanical_collection/src/Controller/SubprojectsController.php
+++ pivity_botanical_collection/src/Controller/SubprojectsController.php
@@ -5,14 +5,58 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Subproject;
+use App\Repository\SubprojectRepository;
+use App\Repository\ProjectRepository;
 
 class SubprojectsController extends AbstractController
 {
+    private string $message = "";
+    private array $subprojectObjects = [];
+    private array $projectObjects = [];
+
     #[Route('/subprojects', name: 'subprojects')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $subprojects = SubprojectRepository::readAllSubprojects($doctrine);
+        $projects = ProjectRepository::readAllProjects($doctrine);
+
+        if($projects){
+            $this->projectObjects = $projects;
+        }
+
+        if($subprojects){
+
+            $this->subprojectObjects = $subprojects;
+
+            return $this->render('subprojects/index.html.twig', [
+                'controller_name' => 'ProjectsController',
+                'message' => $this->message,
+                'subprojects' => $this->subprojectObjects,
+                'projects' => $this->projectObjects,
+            ]);
+        }
+
         return $this->render('subprojects/index.html.twig', [
-            'controller_name' => 'SubprojectsController',
+            'controller_name' => 'ProjectsController',
+            'message' => $this->message = "You have no subprojects yet!",
+            'subprojects' => $this->subprojectObjects,
+            'projects' => $this->projectObjects,
         ]);
+
+    }
+
+    #[Route('/createSubproject', name: 'createSubproject')]
+    public function createSubproject(ManagerRegistry $doctrine, Request $request,): RedirectResponse
+    {
+        $subprojectName = $request->request->get('subprojectName');
+        $projectId = $request->request->get('project');
+
+        var_dump($subprojectName, $projectId);
+
+        return $this->redirectToRoute('subprojects'); 
     }
 }
